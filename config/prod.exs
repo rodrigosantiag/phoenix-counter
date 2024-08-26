5ae29d652d1ee55d0056142e686b9e952811f784
Rodrigo Santiago <rodrigosantiag@gmail.com>
2024-08-26 14:20:33 -0300

    Try to deploy to production

--- config/prod.exs
+++ config/prod.exs
@@ -6,6 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :counter_by_myself, CounterByMyselfWeb.Endpoint,
+  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
---
+++ rel/overlays/bin/server.bat
@@ -0,0 +1,2 @@
+set PHX_SERVER=true
+call "%~dp0\counter_by_myself" start
